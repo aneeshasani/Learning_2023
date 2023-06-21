e9043b3c5fb54b179dfaa653cd4022ac1888b75b
aneeshasani <aneeshasani2001@gmail.com>
2023-06-21 20:38:32 +0530

    module1day5

--- Module1/Day5/prog2.c
+++ Module1/Day5/prog2.c
@@ -7,31 +7,27 @@ struct complex
    struct complex read(void);
    void write(struct complex);
    struct complex add(struct complex, struct complex);
-   struct complex sub(struct complex, struct complex);
    struct complex mul(struct complex, struct complex);
-   struct complex div(struct complex, struct complex);
 void main ()
 {
    clrscr();
    printf("Enter the 1st complex number\n ");
    a = read();
    write(a);
+
    printf("Enter  the 2nd complex number\n");
    b = read();
    write(b);
+
    printf("Addition\n ");
    c = add(a, b);
    write(c);
-   printf("Substraction\n ");
-   c = sub(a, b);
-   write(c);
+
    printf("Multiplication\n");
    c = mul(a, b);
    write(c);
-   printf("Division\n");
-   c = div(a, b);
-  write(c);
-  getch();
+
+   getch();
 }
 struct complex read(void)
 {
@@ -55,24 +51,10 @@ struct complex add(struct complex p, struct complex q)
    t.imag = (p.imag + q.imag);
    return t;
 }
-struct complex sub(struct complex p, struct complex q)
-{
-   struct complex t;
-   t.real = (p.real - q.real);
-   t.imag = (p.imag - q.imag);
-   return t;
-}
 struct complex mul(struct complex p, struct complex q)
 {
    struct complex t;
    t.real=(p.real * q.real) - (p.imag * q.imag);
    t.imag=(p.real * q.imag) + (p.imag * q.real);
    return t;
-}
-struct complex div(struct complex p, struct complex q)
-{
-   struct complex t;
-   t.real = ((p.imag * q.real) - (p.real * q.imag)) / ((q.real * q.real) + (q.imag * q.imag));
-   t.imag = ((p.real * q.real) + (p.imag * q.imag)) / ((q.real * q.real) + (q.imag * q.imag));
-   return(t);
 }
